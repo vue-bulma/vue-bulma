b0568631e947e3bc00f3396eb097ed3f4a3a71cf
Fangdun Cai <cfddream@gmail.com>
2016-12-08 15:21:59 +0800

    Elements: improve Button

--- src/elements/Button.vue
+++ src/elements/Button.vue
@@ -1,13 +1,50 @@
 <template>
-  <button>
+  <button :class="classes">
     <slot></slot>
   </button>
 </template>
 
 <script>
 export default {
+  props: {
+    type: {
+      type: String,
+      default: '',
+      validator: value => ['primary', 'info', 'success', 'warning', 'danger', 'link'].includes(value)
+    },
+    size: {
+      type: String,
+      default: '',
+      validator: value => ['small', 'medium', 'large'].includes(value)
+    },
+    style: {
+      type: String,
+      default: '',
+      validator: value => ['outlined', 'inverted'].includes(value)
+    },
+    state: {
+      type: String,
+      default: '',
+      validator: value => ['loading', 'active', 'disabled'].includes(value)
+    }
+  },
+
+  computed: {
+    classes () {
+      const { type, size, style, state } = this
+
+      return {
+        button: true,
+        [`is-${type}`]: !!type,
+        [`is-${size}`]: !!size,
+        [`is-${style}`]: !!style,
+        [`is-${state}`]: !!state
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss">
+@import '~bulma/sass/elemens/button.sass';
 </style>

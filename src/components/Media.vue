cc94ac8df432030b2fc50ee61ab0554f5f78ce9c
Luventa <silentmic@qq.com>
2017-01-16 14:41:50 +0800

    components: Media
    Implement component Media.
    Refactor of Box.
    Import image.sass to index.scss.

---
+++ src/components/Media.vue
@@ -0,0 +1,19 @@
+<template>
+  <article class="media">
+    <slot></slot>
+    <div class="media-left">
+      <slot name="media-left"></slot>
+    </div>
+    <div class="media-content">
+      <slot name="media-content"></slot>
+    </div>
+    <div class="media-right">
+      <slot name="media-right"></slot>
+    </div>
+  </article>
+</template>
+
+<style lang="scss">
+@import '~bulma/sass/utilities/_all';
+@import '~bulma/sass/components/media.sass';
+</style>
--- src/elements/Box.vue
+++ src/elements/Box.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="box">
-    <article class="media">
-      <div class="media-left">
-        <slot name="media-left"></slot>
-      </div>
-      <div class="media-content">
-        <slot></slot>
-      </div>
-    </article>
+    <slot></slot>
   </div>
 </template>
 

0221320e097380707b2f6f66d9227baa4406feae
Luventa <silentmic@qq.com>
2017-01-26 10:35:14 +0800

    components: Message

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import { Level, LevelItem } from './Level'
 import Media from './Media.vue'
+import Message from './Message.vue'
 
 const vueBulmaComponents = {
   VueBulmaLevel: Level,
   VueBulmaLevelItem: LevelItem,
-  VueBulmaMedia: Media
+  VueBulmaMedia: Media,
+  VueBulmaMessage: Message
 }
 
 export default vueBulmaComponents

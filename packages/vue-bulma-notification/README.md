11efbd5f4b1437401d7a511847c64121be571afd
LianHe Cai <minnancaitou@gmail.com>
2019-01-21 17:02:45 +0800

    add vue-bulma-notification

---
+++ packages/vue-bulma-notification/README.md
@@ -0,0 +1,11 @@
+# `vue-bulma-notification`
+
+> TODO: description
+
+## Usage
+
+```
+const vueBulmaNotification = require('vue-bulma-notification');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/vue-bulma-notification/src/main.js
@@ -0,0 +1,47 @@
+import colorProps from 'vue-bulma-common/src/mixins/color.js'
+
+export default {
+  name: 'vb-notification',
+  mixins: [colorProps],
+  computed: {
+    classes() {
+      const { color } = this
+      return {
+        notification: true,
+        [`is-${color}`]: !!color
+      }
+    }
+  },
+  methods: {
+    handleClick(event) {
+      this.$emit('click', event)
+    },
+    handleMouseenter(event) {
+      this.$emit('mouseenter', event)
+    },
+    handleMouseleave(event) {
+      this.$emit('mouseleave', event)
+    }
+  },
+  render(h) {
+    const {
+      classes,
+      handleClick,
+      handleMouseenter,
+      handleMouseleave,
+      $slots
+    } = this
+    return h(
+      'div',
+      {
+        class: classes,
+        on: {
+          click: handleClick,
+          mouseenter: handleMouseenter,
+          mouseleave: handleMouseleave
+        }
+      },
+      $slots.default
+    )
+  }
+}
